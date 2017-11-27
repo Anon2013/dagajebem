49e2918f75d911fd2c966391029540c637e30125
Anon <anon2013@github.com>
2017-11-27 18:09:02 +0100

    Aca

---
+++ MackicaSkakackica/MackicaSkakackica/Assets/Scripts/movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float highJump = 10.0f;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+        if (Input.GetButton("Jump"))
+        {
+            rb.velocity = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z * Time.deltaTime);
+        }
+	}
+}
